19d28cf12d35e0fe458e1e78b633be16d8628bf0
jpierre06 <jpierre06@gmail.com>
2025-06-28 19:08:46 -0300

    change: Adiciona medidas estatísticas na função describe_full_df

--- scripts/local_tools.py
+++ scripts/local_tools.py
@@ -1,3 +1,4 @@
+from typing import Literal
 import jupyterlab
 import ipykernel
 
@@ -49,29 +50,103 @@ def mode_count(list_values:list):
     return mode(list_values, True)
 
 
-def describe_full_df(_df: pd.DataFrame, expand_mode=True):
+def mean_abs_deviation(list_values: list):
+    mean = np.mean(list_values)
+    mean_ad = np.mean(abs(list_values - mean))
+    return mean_ad
+
+
+def median_abs_deviation_norm(list_values: list):
+    mad = stats.median_abs_deviation(list_values)
+    factor = 1 / stats.norm.ppf(0.75)
+    mad_norm = mad * factor  # Factor for normal distribution
+    return mad_norm
+
+
+def outlier_values(list_values: list, dtype: Literal['lower', 'upper']):
+    q1 = np.percentile(list_values, 25)
+    q3 = np.percentile(list_values, 75)
+    iqr = q3 - q1
+    
+    if dtype == 'lower':
+        outlier = q1 - 1.5 * iqr
+    elif dtype == 'upper':
+        outlier = q3 + 1.5 * iqr
+    else:
+        outlier = None
+        
+    return outlier
+
+
+def count_outlier_values(list_values: list, dtype: Literal['lower', 'upper']):
+    if dtype == 'lower':
+        outlier = outlier_values(list_values, dtype)
+        lower_outliers = list_values[list_values < outlier]
+        num = len(lower_outliers)
+    elif dtype == 'upper':
+        outlier = outlier_values(list_values, dtype)
+        upper_outliers = list_values[list_values > outlier]
+        num = len(upper_outliers)
+    else:
+        num = None
+        
+    return num
+
+
+def describe_full_df(_df: pd.DataFrame):
     '''Generates a full description of a DataFrame, 
     including additional statistics for numeric columns.
-    If expand_mode is True, it will include mode and its variations.
     '''
 
     cols_number = _df.select_dtypes(include='number').columns
     df_describe = _df.describe()
-
+    
+    percentile_25 = lambda x: np.percentile(x, 25)
+    percentile_75 = lambda x: np.percentile(x, 75)
+    range_values = lambda x: np.max(x) - np.min(x)
+    lower_outlier = lambda x: outlier_values(x, 'lower')
+    upper_outlier = lambda x: outlier_values(x, 'upper')
+    count_lower_outlier = lambda x: count_outlier_values(x, 'lower')
+    count_upper_outlier = lambda x: count_outlier_values(x, 'upper')
+    coefficient_variation = lambda x: pd.Series.std(x) / pd.Series.mean(x)
+    shapiro_stat = lambda x: stats.shapiro(x)[0]
+    shapiro_pvalue = lambda x: stats.shapiro(x)[1]
+    
+    
     if np.all(df_describe.columns == cols_number):
 
         dict_functions = {
+            'count': pd.Series.count,
             'count_zero': count_zero,
             'count_nonzero': np.count_nonzero,
             'count_unique': pd.Series.nunique,
+            'mean': pd.Series.mean,
             'median': pd.Series.median,
-            'median_abs_deviation': stats.median_abs_deviation,
-            'iqr': stats.iqr,
             'mode': mode,
             'mode_lists': mode_lists,
             'mode_count': mode_count,
+            'min': np.min,
+            '25%': percentile_25,
+            '50%': pd.Series.median,
+            '75%': percentile_75,
+            'max': np.max,
+            'range_values': range_values,
+            'iqr': stats.iqr,
+            'lower_outlier': lower_outlier,
+            'count_lower_outlier': count_lower_outlier,
+            'upper_outlier': upper_outlier,
+            'count_upper_outlier': count_upper_outlier,
+            'mean_abs_deviation': mean_abs_deviation,
+            'std': pd.Series.std,
+            'median_abs_deviation': stats.median_abs_deviation,
+            'median_abs_deviation_norm': median_abs_deviation_norm,
+            'coefficient_variation': coefficient_variation, 
+            'variance': np.var,
             'kurt': pd.Series.kurt,
+            'kurtosis': stats.kurtosis,
             'skew': pd.Series.skew,
+            'shapiro_stat': shapiro_stat,
+            'shapiro_pvalue': shapiro_pvalue,
             'autocorr': pd.Series.autocorr,
             'circvar': stats.circvar,
             'circmean': stats.circmean,
@@ -93,9 +168,9 @@ def describe_full_df(_df: pd.DataFrame, expand_mode=True):
         df_temp = pd.DataFrame(dict_data)
         df_temp.index = dict_functions.keys()
 
-        return pd.concat([df_describe, df_temp])
-
-
+        return pd.concat([df_temp])
+    
+    
 def save_profile_report(df: pd.DataFrame, filename: str, title: str = "Pandas Profiling Report"):
     '''Saves a profile report of a DataFrame to an HTML file.'''
     
